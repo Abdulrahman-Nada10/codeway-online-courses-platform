c04ad6bf5e08ec2300b535b3a1e1b19dc1bc5ffc
Assem Khashaba <assemkhashaba404@gmail.com>
2026-05-11 18:19:42 +0300

    Revert "Feature/notifications"

--- GlobalResponse.Shared/Configuration/DuplicateProtectionOptions.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace GlobalResponse.Shared;
-
-public class DuplicateProtectionOptions
-{
-    public int TimeToLiveSeconds { get; set; }
-    public Dictionary<string, string> Messages { get; set; } = new();
-    public List<string> ExcludedRoutes { get; set; } = new(); // add excluded paths
-}
-
--- OnlineCourseSystem.Notifications/Infrastructure/Repositories/Interfaces/IUserNotificationDeliveries.cs
+++
@@ -1,9 +0,0 @@
-﻿using OnlineCourseSystem.Notifications.Models;
-
-namespace OnlineCourseSystem.Notifications.Infrastructure.Repositories.Interfaces
-{
-    public interface IUserNotificationDeliveries
-    {
-        Task AddRangeAsync(IEnumerable<UserNotificationDelivery> deliveries, CancellationToken cancellationToken = default);
-    }
-}
